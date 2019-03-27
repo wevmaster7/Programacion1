df59e39e6ff53ca4daa8a16e339c1ba6e9915259
wevmaster7 <47875039+wevmaster7@users.noreply.github.com>
2019-03-26 22:17:54 -0600

    Add files via upload
    clase de operaciones, suma, resta, producto, división.

---
+++ Class26032018/Class1/Division.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class1
+{
+    public class Division
+    {
+
+        private  double v1, v2, resultado;
+    
+        public double Resultado
+        {
+            get { return resultado; }
+            set { resultado = value; }
+        }
+
+        public double V2
+        {
+            get { return v2; }
+            set { v2 = value; }
+        }
+
+        public double V1
+        {
+            get { return v1; }
+            set { v1 = value; }
+        }
+        public Division() 
+        {
+            this.v1 = 0;
+            this.v2 = 0;
+        }
+
+        public double CalcularDivision()
+        {
+
+            Resultado = V1 / V2;
+            return Resultado;
+        }
+    }
+}
